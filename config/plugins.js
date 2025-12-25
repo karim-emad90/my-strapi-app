878bf2b98217c71bb4e49e38997c252923d9a33c
karim-emad90 <karim.sharkawy90@gmail.com>
2025-12-25 03:47:31 +0200

    'cloudnary'

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+ upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("dw0ikxwux"),
+        api_key: env("643692664488691"),
+        api_secret: env("6GfyoSb6xbqEdSh2zi_MwGCS5Vo"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
